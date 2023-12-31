74152452163035eab7f823c23274b3b3895a4e2c
Pablo Valdes <pablovaldes0925@gmail.com>
2023-12-30 19:21:51 -0500

    origin changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["https://general-planner.onrender.com/"];
+const allowedOrigins = [
+  "https://general-planner.onrender.com/",
+  "https://general-planner.onrender.com",
+  "https://www.general-planner.onrender.com/",
+  "https://www.general-planner.onrender.com",
+];
 
 module.exports = allowedOrigins;
